0b18aa328c9edb02ec1e4e2f70e9f30ab5b3bf60
phil.roche <phil.roche@ticket-text.com>
2014-06-09 12:12:02 +0100

    Change version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Django-tinymce-filebrowser',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/aisayko/Django-tinymce-filebrowser',
     license='MIT License',
     author='Alex Isayko',
